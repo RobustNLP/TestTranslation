8e0d7f426fa4d6d087d988da7a93df9644fb894e
Pinjia He <pinjiahe@gmail.com>
2021-02-02 20:52:42 +0100

    Update PatInv.py

--- code/PatInv/PatInv.py
+++ code/PatInv/PatInv.py
@@ -15,6 +15,8 @@ import urllib.request, urllib.error, urllib.parse
 from bs4 import BeautifulSoup
 import re
 import json
+import tensorflow as tf
+import tensorflow_hub as hub
 from apted import APTED
 from apted.helpers import Tree
 from nltk.parse import CoreNLPParser
@@ -22,7 +24,26 @@ from nltk.stem import WordNetLemmatizer
 from nltk.stem import WordNetLemmatizer
 from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import wordnet
-from use import UseSimilarity
+#from use import UseSimilarity
+
+class UseSimilarity(object):
+    def __init__(self, use_dir):
+        
+        self.embed = hub.Module(use_dir)
+        tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+        self.session = tf.Session()
+        self.session.run([tf.global_variables_initializer(), tf.tables_initializer()])
+
+    def get_sim(self, sentences):
+        message_embeddings = self.session.run(self.embed(sentences))
+        arr = np.array(message_embeddings).tolist()
+        print(np.inner(arr[0], arr[1]))
+
+    def __exit__(self):
+        self.session.close()
+
+    def close(self):
+        self.session.close()
 
 def perturb(sentence, bertmodel, num):
 	"""Generate a list of similar sentences by BERT
